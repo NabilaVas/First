1049f87ee6152fbbe90789fa260f9ca5fff17072
NabilaVas <122089241+NabilaVas@users.noreply.github.com>
2023-01-06 15:33:15 +0530

    Welcome with name

--- Welcome.java
+++ Welcome.java
@@ -1,7 +1,10 @@
+import java.util.*;
 class Welcome
 {
   public static void main(String[] args)
   {
-    System.out.println("Hello World");
+    Scanner in=new Scanner(System.in);
+    String name=in.nextLine();
+    System.out.println("Welcome "+name);
   }
 }

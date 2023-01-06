28ecb6bbf117bd7b9465d77967b061def6ab50ef
NabilaVas <122089241+NabilaVas@users.noreply.github.com>
2023-01-06 15:40:49 +0530

    Print details

---
+++ Print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Print
+{
+  public static void main(String[] args)
+  {
+    Scanner in=new Scanner(System.in);
+    String name=in.nextLine();
+    int age=in.nextInt();
+    System.out.println("Name :"+name+"\nAge :"+age);
+  }
+}
